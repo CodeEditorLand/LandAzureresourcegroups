8150901d5c397fac4704a3d473037309137728d7
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-26 14:18:18 +0300

    squash!

--- extension.bundle.ts
+++ extension.bundle.ts
@@ -14,34 +14,34 @@
 //
 // The tests should import '../extension.bundle'. At design-time they live in tests/ and so will pick up this file (extension.bundle.ts).
 // At runtime the tests live in dist/tests and will therefore pick up the main webpack bundle at dist/extension.bundle.js.
-export * from '@microsoft/vscode-azext-utils';
-export * from './api/src/AzExtResourceType';
+export * from "@microsoft/vscode-azext-utils";
+export * from "./api/src/AzExtResourceType";
 // export * from './api/src';
-export * from './api/src/extensionApi';
-export * from './api/src/resources/azure';
-export * from './api/src/resources/base';
-export * from './api/src/resources/workspace';
-export * from './api/src/utils/getApi';
-export * from './api/src/utils/wrapper';
-export * from './src/api/DefaultAzureResourceProvider';
-export { convertV1TreeItemId } from './src/api/compatibility/CompatibleAzExtTreeDataProvider';
-export * from './src/commands/openInPortal';
-export * from './src/commands/tags/getTagDiagnostics';
-export * from './src/commands/viewProperties';
-export * from './src/services/AzureResourcesService';
+export * from "./api/src/extensionApi";
+export * from "./api/src/resources/azure";
+export * from "./api/src/resources/base";
+export * from "./api/src/resources/workspace";
+export * from "./api/src/utils/getApi";
+export * from "./api/src/utils/wrapper";
+export * from "./src/api/DefaultAzureResourceProvider";
+export { convertV1TreeItemId } from "./src/api/compatibility/CompatibleAzExtTreeDataProvider";
+export * from "./src/commands/openInPortal";
+export * from "./src/commands/tags/getTagDiagnostics";
+export * from "./src/commands/viewProperties";
+export * from "./src/services/AzureResourcesService";
 // Export activate/deactivate for main.js
-export { activate, deactivate } from './src/extension';
-export * from './src/extensionVariables';
-export * from './src/hostapi.v2.internal';
-export * from './src/tree/BranchDataItemWrapper';
-export * from './src/tree/InvalidItem';
-export { ResourceGroupsItem } from './src/tree/ResourceGroupsItem';
-export * from './src/tree/azure/AzureResourceItem';
-export * from './src/tree/azure/SubscriptionItem';
-export { createSubscriptionContext as createSubscriptionContext2 } from './src/tree/azure/VSCodeAuthentication';
-export * from './src/tree/azure/grouping/GroupingItem';
-export * from './src/tree/azure/grouping/LocationGroupingItem';
-export * from './src/tree/azure/grouping/ResourceGroupGroupingItem';
-export * from './src/tree/azure/grouping/ResourceTypeGroupingItem';
-export * from './src/utils/wrapFunctionsInTelemetry';
+export { activate, deactivate } from "./src/extension";
+export * from "./src/extensionVariables";
+export * from "./src/hostapi.v2.internal";
+export * from "./src/tree/BranchDataItemWrapper";
+export * from "./src/tree/InvalidItem";
+export { ResourceGroupsItem } from "./src/tree/ResourceGroupsItem";
+export * from "./src/tree/azure/AzureResourceItem";
+export * from "./src/tree/azure/SubscriptionItem";
+export { createSubscriptionContext as createSubscriptionContext2 } from "./src/tree/azure/VSCodeAuthentication";
+export * from "./src/tree/azure/grouping/GroupingItem";
+export * from "./src/tree/azure/grouping/LocationGroupingItem";
+export * from "./src/tree/azure/grouping/ResourceGroupGroupingItem";
+export * from "./src/tree/azure/grouping/ResourceTypeGroupingItem";
+export * from "./src/utils/wrapFunctionsInTelemetry";
 // NOTE: The auto-fix action "source.organizeImports" does weird things with this file, but there doesn't seem to be a way to disable it on a per-file basis so we'll just let it happen
